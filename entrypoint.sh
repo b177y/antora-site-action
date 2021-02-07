552d29fee787979021e735a4e798ecae92d686d8
b177y <34008579+b177y@users.noreply.github.com>
2021-02-07 18:27:49 +0000

    Test

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,9 @@ cd $GITHUB_WORKSPACE/$INPUT_SITE_SOURCES_PATH
 
 ls -ltr $GITHUB_WORKSPACE/$INPUT_SITE_SOURCES_PATH
 
+git worktree add -B gh-pages build/site origin/gh-pages
 antora --stacktrace $INPUT_ANTORA_PLAYBOOK
+
+cd build/site
+git add .
+git commit -m "Test build static site to gh-pages"
